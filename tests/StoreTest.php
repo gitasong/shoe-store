f1c8f52daae94fa5985f890ff349a759bf234f29
Nicole Freed <nicole.freed@gmail.com>
2017-07-21 10:17:50 -0700

    change 'Macy\'s' to 'Macys'

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -15,15 +15,16 @@
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
-        // protected function tearDown()
-        // {
-        //     Store::deleteAll();
-        //     Brand::deleteAll();
-        // }
+
+        protected function tearDown()
+        {
+            Store::deleteAll();
+            // Brand::deleteAll();
+        }
 
         function testGetStoreName()
         {
-            $store_name = "Macy's";
+            $store_name = "Macys";
             $test_store = new Store($store_name);
 
             $result = $test_store->getStoreName();
@@ -53,48 +54,48 @@
             $this->assertTrue($executed, "Store not successfully saved to database");
         }
 
-        // function testGetID()
-        // {
-        //     $store_name = "Famous Brands";
-        //     $test_store = new Store($store_name);
-        //     $test_store->save();
-        //
-        //     $result = $test_store->getID();
-        //
-        //     $this->assertEquals(true, is_numeric($result));
-        // }
-        //
-        // function testGetAll()
-        // {
-        //     $store_name_1 = "Macy's";
-        //     $test_store_1 = new Store($store_name_1);
-        //     $test_store_1->save();
-        //
-        //     $store_name_2 = "TJ Maxx";
-        //     $test_store_2 = new Store($store_name_2);
-        //     $test_store_2->save();
-        //
-        //     $result = Store::getAll();
-        //
-        //     $this->assertEquals([$test_store_1, $test_store_2], $result);
-        // }
-        //
-        // function testDeleteAll()
-        // {
-        //     $store_name_1 = "Famous Brands";
-        //     $test_store_1 = new Store($store_name_1);
-        //     $test_store_1->save();
-        //
-        //     $store_name_2 = "Top Shoes";
-        //     $test_store_2 = new Store($store_name_2);
-        //     $test_store_2->save();
-        //
-        //     Store::deleteAll();
-        //     $result = Store::getAll();
-        //
-        //     $this->assertEquals([], $result);
-        // }
-        //
+        function testGetID()
+        {
+            $store_name = "Famous Brands";
+            $test_store = new Store($store_name);
+            $test_store->save();
+
+            $result = $test_store->getID();
+
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function testGetAll()
+        {
+            $store_name_1 = "Macys";
+            $test_store_1 = new Store($store_name_1);
+            $test_store_1->save();
+
+            $store_name_2 = "TJ Maxx";
+            $test_store_2 = new Store($store_name_2);
+            $test_store_2->save();
+
+            $result = Store::getAll();
+
+            $this->assertEquals([$test_store_1, $test_store_2], $result);
+        }
+
+        function testDeleteAll()
+        {
+            $store_name_1 = "Famous Brands";
+            $test_store_1 = new Store($store_name_1);
+            $test_store_1->save();
+
+            $store_name_2 = "Top Shoes";
+            $test_store_2 = new Store($store_name_2);
+            $test_store_2->save();
+
+            Store::deleteAll();
+            $result = Store::getAll();
+
+            $this->assertEquals([], $result);
+        }
+
         // function testFind()
         // {
         //     $store_name_1 = "Famous Brands";
@@ -124,7 +125,7 @@
         //
         // function testDelete()
         // {
-        //     $store_name_1 = "Macy's";
+        //     $store_name_1 = "Macys";
         //     $test_store_1 = new Store($store_name_1);
         //     $test_store_1->save();
         //
@@ -174,7 +175,7 @@
         //
         // function testFindStoreByName()
         // {
-        //     $store_name_1 = "Macy's";
+        //     $store_name_1 = "Macys";
         //     $test_store_1 = new Store($store_name_1);
         //     $test_store_1->save();
         //
