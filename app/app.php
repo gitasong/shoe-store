e84c75fcbe445027f888745b2a7d3b644e8bc0da
Nicole Freed <nicole.freed@gmail.com>
2017-07-21 11:32:52 -0700

    add boilerplate to app/app.php with basic root route ('/')

---
+++ app/app.php
@@ -0,0 +1,30 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=shoe_store';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('stores.html.twig');
+    });
+
+    return $app;
+?>
