5b0db94e7ac0acbd84042c8720749a48c7718dd4
Nicole Freed <nicole.freed@gmail.com>
2017-07-21 10:18:27 -0700

    pass tests for Store->getAll() and Store->deleteAll()

--- src/Store.php
+++ src/Store.php
@@ -33,29 +33,29 @@
             }
         }
 
-        // function getID()
-        // {
-        //     return $this->id;
-        // }
-        //
-        // static function getAll()
-        // {
-        //     $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores");
-        //     $stores = array();
-        //     foreach($returned_stores as $store) {
-        //         $store_store_name = $store['store_name'];
-        //         $store_id = $store['id'];
-        //         $new_store = new Store($store_store_name, $store_id);
-        //         array_push($stores, $new_store);
-        //     }
-        //     return $stores;
-        // }
-        //
-        // static function deleteAll()
-        // {
-        //     $GLOBALS['DB']->exec("DELETE FROM stores;");
-        // }
-        //
+        function getID()
+        {
+            return $this->id;
+        }
+
+        static function getAll()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores");
+            $stores = array();
+            foreach($returned_stores as $store) {
+                $store_name = $store['store_name'];
+                $store_id = $store['id'];
+                $new_store = new Store($store_name, $store_id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores;");
+        }
+
         // static function find($search_id)
         // {
         //     $returned_stores = $GLOBALS['DB']->prepare("SELECT * FROM stores WHERE id = :id");
