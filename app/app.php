6d382d20775f653a7c2ca74910b67d4c747c5c29
Nicole Freed <nicole.freed@gmail.com>
2017-07-21 18:14:43 -0700

    add 'all_stores' variable to root route ('/')

--- app/app.php
+++ app/app.php
@@ -1,15 +1,15 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Brand.php";
     require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
 
     $server = 'mysql:host=localhost:8889;dbname=shoe_store';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
-
-    use Symfony\Component\Debug\Debug;
-    Debug::enable();
 
     $app = new Silex\Application();
 
@@ -23,8 +23,9 @@
     Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('stores.html.twig');
+        return $app['twig']->render('stores.html.twig', array('all_stores' => Store::getAll()));
     });
 
     return $app;
+
 ?>
